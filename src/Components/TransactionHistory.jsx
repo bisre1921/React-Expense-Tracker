574f40618f058ae1fcb4b73b8b5d60e91b128182
bisre1921 <bisrattewodros3@gmail.com>
2024-02-29 21:45:56 +0300

    Refactor transaction history using state transactions

--- src/Components/TransactionHistory.jsx
+++ src/Components/TransactionHistory.jsx
@@ -1,28 +1,19 @@
-import { MdDelete } from "react-icons/md";
+import React , { useContext } from "react";
+import {GlobalContext} from "../Context/GlobalState";
+import Transactions from "./Transactions";
+
 const TransactionHistory = () => {
+  const {transactions} = useContext(GlobalContext);
+
   return (
     <div>
         <h1>
           <h3 className="text-lg font-semibold">History</h3>
           <ul className="flex flex-col">
-            <li className="relative bg-white mb-2 flex justify-between">
-              cash  <span>-$400</span>
-              <button className="absolute bottom-0 left-[-20px]">
-                <MdDelete />
-              </button>
-            </li>
-            <li className="relative bg-white mb-2 flex justify-between">
-              food  <span>-$400</span>
-              <button className="absolute bottom-0 left-[-20px]">
-                <MdDelete />
-              </button>
-            </li>
-            <li className="relative bg-white mb-2 flex justify-between">
-              game  <span>-$100</span>
-              <button className="absolute bottom-0 left-[-20px]">
-                <MdDelete />
-              </button>
-            </li>
+            {transactions.map(transaction => (
+              <Transactions key={transaction.id} transaction = {transaction} />
+            ))}
+            
           </ul>
         </h1>
     </div>
---
+++ src/Components/Transactions.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import { MdDelete } from "react-icons/md";
+
+const Transactions = ({transaction}) => {
+  const sign = transaction.amount < 0 ? "-" : "+";
+  return (
+    <div>
+        <li className={`relative bg-white mb-2 pr-2 flex justify-between ${transaction.amount < 0 ? "border-r-4 border-red-600" : "border-r-4 border-green-600"}`}>
+                {transaction.text}  <span> {sign}${ Math.abs(transaction.amount)} </span>
+                <button className="absolute bottom-0 left-[-20px]">
+                  <MdDelete className='hover:text-red-600' />
+                </button>
+        </li>
+    </div>
+  )
+}
+
+export default Transactions
